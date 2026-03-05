b9969acfc430f262bd9644c1bbccfa834c47d339
hanakhalda11-del <hanakhalda11@gmail.com>
2026-03-05 18:38:51 +0700

    sort array

--- bubble_sort.cpp
+++ bubble_sort.cpp
@@ -57,3 +57,25 @@ void bubbleSortArray()
 
     } while (pass <= n - 1);
 }
+void display()
+{
+    cout << endl;
+    cout << "==========" << endl;
+    cout << "Elemen Array yang telah tersusun" << endl;
+    cout << "==========" << endl;
+    cout << endl;
+    for (int j = 0; j < n; j++)
+    {
+        cout << arr[k];
+        if (j < n)
+        {
+            cout << " --> ";
+        }
+    }
+    cout << endl;
+    cout << endl;
+
+    cout << "Jumlah pass = " << n - 1 << endl;
+    cout << endl;
+    cout << endl;
+}
